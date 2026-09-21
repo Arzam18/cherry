76c2d8bf266fe1f3a61eaa4250274905ccd65ff5
Arzam18 <98748640+Arzam18@users.noreply.github.com>
2026-09-21 16:02:20 +0800

    Update mod.rs

--- cherry-core/src/board/move_gen/mod.rs
+++ cherry-core/src/board/move_gen/mod.rs
@@ -1,9 +1,15 @@
-#[cfg(all(target_feature = "avx2", not(target_feature = "avx512f")))]
+#[cfg(all(target_arch = "x86_64", target_feature = "avx2", not(target_feature = "avx512f")))]
 mod avx2;
-#[cfg(target_feature = "avx512f")]
+#[cfg(all(target_arch = "x86_64", target_feature = "avx512f"))]
 mod avx512;
 
-#[cfg(all(target_feature = "avx2", not(target_feature = "avx512f")))]
+#[cfg(target_arch = "aarch64")]
+mod neon;
+
+#[cfg(all(target_arch = "x86_64", target_feature = "avx2", not(target_feature = "avx512f")))]
 pub use avx2::*;
-#[cfg(target_feature = "avx512f")]
+#[cfg(all(target_arch = "x86_64", target_feature = "avx512f"))]
 pub use avx512::*;
+
+#[cfg(target_arch = "aarch64")]
+pub use neon::*;
